69226222ad16876b191cc70b1775c8e01eab7910
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-07-09 14:41:44 +0530

    new

--- counseling_app/adapters.py
+++ counseling_app/adapters.py
@@ -3,19 +3,39 @@ from allauth.exceptions import ImmediateHttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.conf import settings
+from django.contrib.auth import login
 from .models import AdminUser
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         user_email = sociallogin.user.email
         if user_email:
-            # Check if the email is in the authorized list
-            if user_email not in settings.AUTHORIZED_ADMIN_EMAILS:
-                raise ImmediateHttpResponse(redirect(reverse('admin_login') + '?error=unauthorized_google_email'))
-
             try:
                 # Check if an AdminUser with this email already exists
-                AdminUser.objects.get(email=user_email)
+                existing_user = AdminUser.objects.get(email=user_email)
+                # Automatically log in the user if they already exist
+                login(request, existing_user, backend='allauth.account.auth_backends.AuthenticationBackend')
+                raise ImmediateHttpResponse(redirect(settings.LOGIN_REDIRECT_URL))
             except AdminUser.DoesNotExist:
                 # If the email does not exist in AdminUser, prevent login
                 raise ImmediateHttpResponse(redirect(reverse('admin_login') + '?error=unauthorized_email'))
+
+    def get_user_for_social_login(self, request, sociallogin):
+        # Try to find an existing AdminUser with the same email
+        try:
+            return AdminUser.objects.get(email=sociallogin.user.email)
+        except AdminUser.DoesNotExist:
+            return None
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        # Check if an AdminUser with this email already exists
+        try:
+            existing_admin_user = AdminUser.objects.get(email=user.email)
+            # If an existing AdminUser is found, use that one
+            sociallogin.user = existing_admin_user
+            user = existing_admin_user
+        except AdminUser.DoesNotExist:
+            pass # No existing AdminUser, proceed with default allauth user creation/linking
+
+        return user
